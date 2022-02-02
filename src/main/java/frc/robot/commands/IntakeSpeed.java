f48e31bb782f84954516f2546f466424f3fde311
Krish Babu <krishbabu0703@gmail.com>
2022-02-01 19:45:16 -0500

    Krish did this

---
+++ src/main/java/frc/robot/commands/IntakeSpeed.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class IntakeSpeed extends CommandBase {
+  /** Creates a new IntakeSpeed. */
+  private static Intake armIntake;
+  public IntakeSpeed(Intake armIntake) {
+    this.armIntake = armIntake;
+    addRequirements(armIntake);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    armIntake.armIntake(1);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
