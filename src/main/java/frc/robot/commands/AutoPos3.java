511acfe72bd948070835cc72ec62d51798b7ea21
Krish Babu <krishbabu0703@gmail.com>
2022-03-03 19:30:57 -0500

    Position

---
+++ src/main/java/frc/robot/commands/AutoPos3.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Movebackshoot extends SequentialCommandGroup {
+
+  /** Creates a new Movebackshoot. */
+  public Movebackshoot(Intake intake, DriveTrain driveTrain, Arm arm, AHRS gyro) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new AutoDriveForward(4300, driveTrain),
+        new AutoRunIntake(1, intake),
+        new AutoDriveBackward(0, driveTrain), 
+        new GyroLeftTurn(driveTrain, gyro, 180), 
+        new LowerArm(arm),
+        new AutoRunIntake(-0.5, intake),
+        new AutoDriveForward(12285, driveTrain),
+        new GyroLeftTurn(driveTrain, gyro, 0)); 
+  }
+}
