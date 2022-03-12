6406bfac4b9a5360b0a3f8bc21f63295072774db
Vincent keang <Vincentkeang@gmail.com>
2022-03-12 13:04:26 -0500

    Make it so we can switch between camera's

---
+++ src/main/java/frc/robot/commands/ActivateCamera2.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Camera;
+
+public class ActivateCamera2 extends CommandBase {
+  private Camera m_camera;
+  public ActivateCamera2(Camera camera) {
+    m_camera = camera; 
+    addRequirements(m_camera);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  m_camera.ActivateCamera2();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
