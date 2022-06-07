64a80016ed5f1943077c41f8bc295df4f37ceb77
Vincent keang <Vincentkeang@gmail.com>
2022-06-07 17:33:36 -0400

    Forward, Backwards and left and right turns

---
+++ src/main/java/frc/robot/commands/AutoDriveForward.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+public class AutoDriveForward extends CommandBase {
+  private double m_distance;
+  private DriveTrain m_driveTrain;
+  /** Creates a new AutoDriveForward. */
+  public AutoDriveForward(double Distance, DriveTrain driveTrain) {
+    m_distance = Distance;
+    m_driveTrain = driveTrain;
+    // Use addRequirements() here to declare subsystem dependencies.''
+    addRequirements(driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_driveTrain.resetEncoders();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // m_driveTrain.tankDrive(-0.5, -0.5);
+    m_driveTrain.moveDistanceWithPID(m_distance);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    // m_driveTrain.tankDrive(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    boolean isFinished = false;
+    isFinished = m_driveTrain.getAverageEncoderDistance() < m_distance;
+    return isFinished;
+  }
+}
