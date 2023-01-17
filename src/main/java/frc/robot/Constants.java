c4a4509d6f2726a07894c6d8df8562f5b1b81723
david-lemasurier <david.lemasurier@gmail.com>
2023-01-17 17:53:34 -0500

    latest updates

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,44 +14,86 @@ package frc.robot;
  */
 public final class Constants {
 
-  /**
-   * Types of motor controllers
-   *
-   * <p>kCTRE represents {@link com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX} with a {@link
-   * com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX} follower
-   *
-   * <p>kREV represents {@link com.revrobotics.CANSparkMax} with a {@link
-   * com.revrobotics.CANSparkMax} follower
-   */
-  public static final class MotorControllerType {
-    public static final int kCTRE = 1;
-    public static final int kREV = 2;
-    public static final int kHybrid = 3;
-  }
-
-  public static final class MotorControllerDeviceID {
-    public static final int leftLeadDeviceID = 23;
-    public static final int leftFollowerDeviceID = 20;
-
-    public static final int rightLeadDeviceID = 22;
-    public static final int rightFollowerDeviceID = 21;
-  }
-
-  public static final double driveTrainGearRatio = 10.71;
-  public static final double driveTrainWheelDiameter = 6;
-
-  public static final class DrivePIDCoeffients {
-    public static final double kP = 0.1;
-    public static final double kI = 0;
-    public static final double kD = 0;
-    public static final double kIz = 0;
-    public static final double kFF = 0;
-    public static final double kMaxOutput = 0.5;
-    public static final double kMinOutput = -0.5;
+  public static final class AutonomousStartPosition {
+    public static final int position1 = 1;
+    public static final int position2 = 2;
   }
 
   public static final class MotorControllerIdleModes {
     public static final int kBrake = 0;
     public static final int kCoast = 1;
   }
+
+  // Swerve Drive Stuff
+  /**
+   * The left-to-right distance between the drivetrain wheels
+   *
+   * <p>Should be measured from center to center.
+   */
+  public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.632; // 24.875 inches
+  /**
+   * The front-to-back distance between the drivetrain wheels.
+   *
+   * <p>Should be measured from center to center.
+   */
+  public static final double DRIVETRAIN_WHEELBASE_METERS = 0.632; // 24.875 inches
+
+  // public static final int DRIVETRAIN_PIGEON_ID = 0;
+
+  public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 10;
+  public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 11;
+  public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 12;
+  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(310.78);
+
+  public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 20;
+  public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 21;
+  public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 22;
+  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(211.47);
+
+  public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 30;
+  public static final int BACK_LEFT_MODULE_STEER_MOTOR = 31;
+  public static final int BACK_LEFT_MODULE_STEER_ENCODER = 32;
+  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(87.80);
+
+  public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 40;
+  public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 41;
+  public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 42;
+  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(278.13);
+  /**
+   * The maximum voltage that will be delivered to the drive motors.
+   *
+   * <p>This can be reduced to cap the robot's maximum speed. Typically, this is useful during
+   * initial testing of the robot.
+   */
+  public static final double MAX_VOLTAGE = 3; // 12.0;
+
+  /**
+   * The maximum velocity of the robot in meters per second.
+   *
+   * <p>This is a measure of how fast the robot should be able to drive in a straight line.
+   */
+  public static final double MAX_VELOCITY_METERS_PER_SECOND =
+      5880.0
+          / 60.0
+          / ((14.0 / 50.0)
+              * (25.0 / 19.0)
+              * (15.0 / 45.0)) // SdsModuleConfigurations.MK4I_L1.getDriveReduction()
+          * 0.10033 // SdsModuleConfigurations.MK4I_L1.getWheelDiameter()
+          * Math.PI;
+  /**
+   * The maximum angular velocity of the robot in radians per second.
+   *
+   * <p>This is a measure of how fast the robot can rotate in place.
+   */
+  // Here we calculate the theoretical maximum angular velocity. You can also
+  // replace this with a measured amount.
+  public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND =
+      MAX_VELOCITY_METERS_PER_SECOND
+          / Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
+
+
+  public static final class DriverOrientation {
+    public static final int kDriver = 0;
+    public static final int kField = 1;
+  }  
 }
---
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+import java.util.function.DoubleSupplier;
+
+public class SwerveDriveCommand extends CommandBase {
+
+  private final Drivetrain m_driveTrain;
+  private final XboxController m_joystick;
+  private DoubleSupplier m_translationXSupplier;
+  private DoubleSupplier m_translationYSupplier;
+  private DoubleSupplier m_rotationSupplier;
+
+  /** Creates a new SwerveDriveCommand. */
+  public SwerveDriveCommand(Drivetrain driveTrain, XboxController joystick) {
+    m_joystick = joystick;
+    m_driveTrain = driveTrain;
+
+    m_translationXSupplier =
+        () -> -modifyAxis(m_joystick.getLeftY()) * Constants.MAX_VELOCITY_METERS_PER_SECOND;
+    m_translationYSupplier =
+        () -> -modifyAxis(m_joystick.getLeftX()) * Constants.MAX_VELOCITY_METERS_PER_SECOND;
+    m_rotationSupplier =
+        () ->
+            -modifyAxis(m_joystick.getRightX()) * Constants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
+
+    addRequirements(driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_driveTrain.swerveDrive(m_translationXSupplier, m_translationYSupplier, m_rotationSupplier);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_driveTrain.stopDrive();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  private static double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+  private static double modifyAxis(double value) {
+    // Deadband
+    value = deadband(value, 0.05);
+
+    // Square the axis
+    value = Math.copySign(value * value, value);
+
+    return value;
+  }
+}
