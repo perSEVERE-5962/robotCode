108b6b40ded264b504df62ae59b8984295914383
Zachary Baron <baronzach962@gmail.com>
2023-01-21 17:07:44 -0500

    Fixed Subsystem

--- src/main/java/frc/robot/subsystems/LineDetector.java
+++ src/main/java/frc/robot/subsystems/LineDetector.java
@@ -1,13 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.sensors.ColorSensor;
 
-public class LineDetector {
+public class LineDetector extends SubsystemBase {
   private ColorSensor Color_Sensor;
   private final String red = "red";
   private final String blue = "blue";
 
+  /** Creates a new LineDetector2. */
   public LineDetector() {
     Color_Sensor = new ColorSensor();
   }
@@ -21,5 +27,9 @@ public class LineDetector {
     // robot.move(x);
     // }
     return false;
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
