6419856287a73001cd7db89c9711288a18880cae
Krish <krishbabu0703@gmail.com>
2023-01-28 13:54:36 -0500

    Created command sequence to test

--- src/main/java/frc/robot/commands/AutoTest.java
+++ src/main/java/frc/robot/commands/AutoTest.java
@@ -4,16 +4,30 @@
 
 package frc.robot.commands;
 
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class AutoTest extends SequentialCommandGroup {
+  private static final Drivetrain Drivetrain = null;
+
   /** Creates a new AutoTest. */
   public AutoTest() {
+
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+      new Move(Drivetrain, 1,0,0), //move forward
+      new Move(Drivetrain, 0, 0, -1), // turn left
+      new Move(Drivetrain, 0, 1, 0), //move right
+      new Move(Drivetrain, -1, 0, 0), //move back
+      new Move(Drivetrain, 1, -1, 0), //move diagonally forward left
+      new Move(Drivetrain, 0, 0, 1) //turn right
+  
+
+    );
   }
 }
