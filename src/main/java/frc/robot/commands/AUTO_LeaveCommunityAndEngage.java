fbc942c8241950ab8223ab8634403600d1a0f68d
Sunk <69110309+Sunketchupm@users.noreply.github.com>
2023-02-09 19:31:17 -0500

    Add command to move past community line
    Also renamed the command groups to be more clear that they are command groups and which type

--- src/main/java/frc/robot/commands/AUTO_LeaveCommunityAndEngage.java
+++ src/main/java/frc/robot/commands/AUTO_LeaveCommunityAndEngage.java
@@ -5,16 +5,23 @@
 package frc.robot.commands;
 
 import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.LineDetector;
+import frc.robot.subsystems.drivetrain.Drivetrain;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class CheckIfEngaged extends SequentialCommandGroup {
-  /** Creates a new CheckIfEngaged. */
-  public CheckIfEngaged(AHRS gyro) {
+public class AUTO_LeaveCommunityAndEngage extends SequentialCommandGroup {
+  /** Creates a new GroupSeqCom_MovePastLineAndBack. */
+  public AUTO_LeaveCommunityAndEngage(Drivetrain drivetrain, LineDetector lineDetector, AHRS gyro) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new IfClimbing(gyro), new IfLevel(gyro));
+    addCommands(
+      new GroupParRace_MoveToLine(drivetrain, lineDetector),
+      new ForwardDistance(drivetrain, -1, 10 /* Unknown so far */), // Moves foward fully past the line
+      new GroupSeqCom_EngageChargingStation(drivetrain, gyro)
+    );
   }
 }
