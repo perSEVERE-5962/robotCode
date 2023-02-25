03fce2e41a2d0a8568414a4916cad0812953e826
scott-johnston <scottj2leahj@gmail.com>
2023-02-25 10:02:45 -0500

    Added ScorePostion1, 2 and 3 also added Reset Positon

--- src/main/java/frc/robot/commands/manipulator/ScorePostion2.java
+++ src/main/java/frc/robot/commands/manipulator/ScorePostion2.java
@@ -4,26 +4,21 @@
 
 package frc.robot.commands.manipulator;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants.LiftConstants;
-import frc.robot.Constants.ReachConstants;
-import frc.robot.Constants.WristConstants;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ScoreConeOnGridPos2 extends SequentialCommandGroup {
-  /** Creates a new ScoreConeOnGridPos2. */
-  public ScoreConeOnGridPos2() {
+public class ScorePostion2 extends SequentialCommandGroup {
+  /** Creates a new ScorePostion2. */
+  public ScorePostion2() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-        new ParallelCommandGroup(
-            // raise wrist & lift
-            new MoveWrist(WristConstants.kRaiseSoftLimit), new MoveLift(LiftConstants.kPos2)),
-        new MoveReach(ReachConstants.kPos2),
-        new MoveWrist(WristConstants.kLowerSoftLimit),
-        new GripperOpen());
+      new MoveLift(0),
+      new MoveReach(0),
+      new MoveReach(0),
+      new GripperOpen()
+    );
   }
 }
--- src/main/java/frc/robot/commands/manipulator/ScorePostion3.java
+++ src/main/java/frc/robot/commands/manipulator/ScorePostion3.java
@@ -4,26 +4,21 @@
 
 package frc.robot.commands.manipulator;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants.LiftConstants;
-import frc.robot.Constants.ReachConstants;
-import frc.robot.Constants.WristConstants;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ScoreConeOnGridPos1 extends SequentialCommandGroup {
-  /** Creates a new ScoreConeOnGridPos1. */
-  public ScoreConeOnGridPos1() {
+public class ScorePostion3 extends SequentialCommandGroup {
+  /** Creates a new ScorePostion3. */
+  public ScorePostion3() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-        new ParallelCommandGroup(
-            // raise wrist & lift
-            new MoveWrist(WristConstants.kRaiseSoftLimit), new MoveLift(LiftConstants.kPos1)),
-        new MoveReach(ReachConstants.kPos1),
-        new MoveWrist(WristConstants.kLowerSoftLimit),
-        new GripperOpen());
+      new MoveLift(0),
+      new MoveReach(0),
+      new MoveWrist(0),
+      new GripperOpen()
+    );
   }
 }
