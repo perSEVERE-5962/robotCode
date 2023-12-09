78143903e80389065469793c1098daf3ddac505d
Default <none@gmail.com>
2023-12-09 14:54:06 -0500

    Add logitech control flight stick support

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -35,13 +35,22 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   private RobotContainer() {
-    m_driveTrain.setDefaultCommand(
+    /*m_driveTrain.setDefaultCommand(
         new DriveCommand(
             m_driveTrain,
             () -> m_driverController.getRawAxis(OIConstants.kDriverYAxis),
             () -> m_driverController.getRawAxis(OIConstants.kDriverXAxis),
-            () -> m_driverController.getRawAxis(OIConstants.kDriverRotAxis),
-            () -> m_driverController.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx)));
+            () -> m_driverController.getRawAxis(OIConstants.kDriverRotAxis_Logitech),
+            () -> m_driverController.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx_Logitech)));*/
+
+    m_driveTrain.setDefaultCommand(
+        new DriveCommandWithThrottle(
+            m_driveTrain,
+            () -> m_driverController.getRawAxis(OIConstants.kDriverYAxis),
+            () -> m_driverController.getRawAxis(OIConstants.kDriverXAxis),
+            () -> m_driverController.getRawAxis(OIConstants.kDriverRotAxis_Logitech),
+            () -> m_driverController.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx_Logitech),
+            () -> m_driverController.getRawAxis(3)));
 
     configureButtonBindings();
   }
---
+++ src/main/java/frc/robot/commands/DriveCommandWithThrottle.java
@@ -0,0 +1,101 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.drivetrain.SwerveSubsystem;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class DriveCommandWithThrottle extends CommandBase {
+
+  private final SwerveSubsystem swerveSubsystem;
+  private final DoubleSupplier xSpdFunction, ySpdFunction, turningSpdFunction, throttle;
+  private final BooleanSupplier fieldOrientedFunction;
+  // private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
+
+  public DriveCommandWithThrottle(
+      SwerveSubsystem swerveSubsystem,
+      DoubleSupplier xSpdFunction,
+      DoubleSupplier ySpdFunction,
+      DoubleSupplier turningSpdFunction,
+      BooleanSupplier fieldOrientedFunction,
+      DoubleSupplier throttle) {
+    this.swerveSubsystem = swerveSubsystem;
+    this.xSpdFunction = xSpdFunction;
+    this.ySpdFunction = ySpdFunction;
+    this.turningSpdFunction = turningSpdFunction;
+    this.fieldOrientedFunction = fieldOrientedFunction;
+    this.throttle = throttle;
+    // this.xLimiter = new
+    // SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+    // this.yLimiter = new
+    // SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+    // this.turningLimiter =
+    // new
+    // SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond);
+    addRequirements(swerveSubsystem);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    // 1. Get real-time joystick inputs
+    double xSpeed = xSpdFunction.getAsDouble() * ((throttle.getAsDouble() + 1) * 0.5);
+    double ySpeed = ySpdFunction.getAsDouble() * ((throttle.getAsDouble() + 1) * 0.5);
+    double turningSpeed = turningSpdFunction.getAsDouble() * ((throttle.getAsDouble() + 1) * 0.5);
+
+    // 2. Apply deadband
+    // xSpeed = Math.abs(xSpeed) > OIConstants.kDeadband ? xSpeed : 0.0;
+    // ySpeed = Math.abs(ySpeed) > /*OIConstants.kDeadband*/ .4 ? ySpeed : 0.0;
+    // turningSpeed = Math.abs(turningSpeed) > OIConstants.kDeadband ? turningSpeed
+    // : 0.0;
+    ySpeed = MathUtil.applyDeadband(ySpeed, 0.15);
+    xSpeed = MathUtil.applyDeadband(xSpeed, 0.15);
+    turningSpeed = MathUtil.applyDeadband(turningSpeed, 0.15); // 0.15 for xbox
+
+    // 3. Make the driving smoother
+    // xSpeed = xLimiter.calculate(xSpeed) *
+    // DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+    // ySpeed = yLimiter.calculate(ySpeed) *
+    // DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+    // turningSpeed =
+    // turningLimiter.calculate(turningSpeed)
+    // * DriveConstants.kTeleDriveMaxAngularSpeedRadiansPerSecond;
+
+    // 4. Construct desired chassis speeds
+    ChassisSpeeds chassisSpeeds;
+    if (fieldOrientedFunction.getAsBoolean()) {
+      // Relative to field
+      chassisSpeeds =
+          ChassisSpeeds.fromFieldRelativeSpeeds(
+              xSpeed, ySpeed * -1, turningSpeed * -1, swerveSubsystem.getRotation2d());
+    } else {
+      // Relative to robot
+      chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed * -1, turningSpeed * -1);
+    }
+
+    
+
+    // 5. Convert chassis speeds to individual module states
+    SwerveModuleState[] moduleStates =
+        DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+
+    // 6. Output each module states to wheels
+    swerveSubsystem.setModuleStates(moduleStates);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    swerveSubsystem.stopModules();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
