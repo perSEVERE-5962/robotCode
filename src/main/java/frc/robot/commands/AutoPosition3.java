4380b8ab743e3849fb4e8b7ab0204702db0d2996
LunaRocco <krishbabu0703@gmail.com>
2024-02-20 19:41:40 -0500

    psoitions created withy code

--- src/main/java/frc/robot/commands/AutoPosition3.java
+++ src/main/java/frc/robot/commands/AutoPosition3.java
@@ -5,15 +5,26 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Notification;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.drivetrain.SwerveSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class AutoPosition3 extends ParallelCommandGroup {
-  /** Creates a new AutoPosition3. */
-  public AutoPosition3() {
+  /** Creates a new AutoPosition1. */
+  public AutoPosition3(SwerveSubsystem swerveSubsystem, Shooter shooter, Feeder feeder, Notification changeLight, Intake intake) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+      new Shoot(shooter, feeder, changeLight),
+      new TurnToZero(swerveSubsystem, 1),
+      new MoveWithTrajectory(swerveSubsystem).getTrajectoryCommandGroup(),
+      new IntakeNote(intake, changeLight, feeder),
+      new Shoot(shooter, feeder, changeLight)
+
+    );
   }
 }
