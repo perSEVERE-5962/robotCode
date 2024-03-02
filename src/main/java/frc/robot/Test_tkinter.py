97a1c0cc4f7d1fd5a3700f52d3d1aa17f6ee99ea
Pranav <sharma.pranav2010@gmail.com>
2024-03-02 11:20:53 -0500

    Make server 5962

--- src/main/java/frc/robot/Test_tkinter.py
+++ src/main/java/frc/robot/Test_tkinter.py
@@ -3,7 +3,7 @@ from networktables import NetworkTables
 
 class userInterface:
    def __init__(self):
-      NetworkTables.initialize(server='10.99.99.2')
+      NetworkTables.initialize(server='10.59.62.2')
       table = NetworkTables.getTable("AutomonusSelect")
       self.someNumberEntry = table.getEntry('Close Note')
       self.someNumberEntry.setDefaultNumber(0)
