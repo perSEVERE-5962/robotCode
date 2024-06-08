f3a4723e1df522e499179ff1520931bff4e19aa8
Pranshu5962 <sharma.pranshu2009@gmail.com>
2024-06-08 15:15:55 -0400

    Added the PhotonVision into the code

---
+++ src/main/java/frc/robot/PhotonVision.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import org.photonvision.PhotonCamera;
+
+/** Add your docs here. */
+public class PhotonVision {
+    PhotonCamera camera = new PhotonCamera("photonvision");
+
+    public static double GettargetDistance(){
+        return 0;
+    }
+
+}
--- src/main/java/frc/robot/commands/ShootWithApriltag.java
+++ src/main/java/frc/robot/commands/ShootWithApriltag.java
@@ -24,7 +24,7 @@ public class ShootWithApriltag extends SequentialCommandGroup {
       //new LogApriltag(),
       new MoveToShootDistance().withTimeout(2),//The shoot with April tag needs to be fixed
       //new LogApriltag(),
-      new TurnToApriltag().withTimeout(2)/*,
+      new TurnToAprilTag().withTimeout(2)/*,
       new SpinUpShooter(1, 1, 0).withTimeout(1),
       new RunShooterFeeder(feeder,notification),
       new StopShooter(shooter)*/
