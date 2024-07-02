42ecb05f269f6943c4114beff6e08edd98e1eb26
Pranshu5962 <sharma.pranshu2009@gmail.com>
2024-07-02 17:51:26 -0400

    fixing the changes to photoncamera

--- src/main/java/frc/robot/PhotonVision.java
+++ src/main/java/frc/robot/PhotonVision.java
@@ -5,11 +5,20 @@
 package frc.robot;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Transform3d;
 
 /** Add your docs here. */
 public class PhotonVision {
-    PhotonCamera camera = new PhotonCamera("photonvision");
-
+    
+    PhotonCamera camera = new PhotonCamera("FrontCamera");
+    AprilTagFieldLayout fieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+    Transform3d camerapose = new Transform3d();
+    PhotonPoseEstimator PoseEstimator = new PhotonPoseEstimator(fieldLayout,PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY,camera,camerapose);
+    
     public static double GettargetDistance(){
         return 0;
     }
