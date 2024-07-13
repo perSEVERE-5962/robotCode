1c42729a6c86d161dc414bef0536c99e9804941e
Pranshu5962 <sharma.pranshu2009@gmail.com>
2024-07-13 14:52:09 -0400

    Worked on Photon Vision
    I worked on getting target data and then using it.

--- src/main/java/frc/robot/PhotonVision.java
+++ src/main/java/frc/robot/PhotonVision.java
@@ -4,23 +4,42 @@
 
 package frc.robot;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.proto.PhotonTrackedTargetProto;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 
 /** Add your docs here. */
 public class PhotonVision {
-    
-    PhotonCamera camera = new PhotonCamera("FrontCamera");
+   private static PhotonCamera cameraFront = new PhotonCamera("FrontCamera");
     AprilTagFieldLayout fieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-    Transform3d camerapose = new Transform3d();
-    PhotonPoseEstimator PoseEstimator = new PhotonPoseEstimator(fieldLayout,PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY,camera,camerapose);
+    PhotonPipelineResult result=cameraFront.getLatestResult();
+    List<PhotonTrackedTarget> targets = result.getTargets();
+    PhotonTrackedTarget target= result.getBestTarget();
+    Transform3d camerapose = new Transform3d(new Translation3d(0,0,0),new Rotation3d(0,0,0));
+    Pose3d robotPose=PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(),fieldLayout.getTagPose(target.getFiducialId()).get(), camerapose);
+
+
     
-    public static double GettargetDistance(){
-        return 0;
-    }
+    PhotonPoseEstimator PoseEstimator = new PhotonPoseEstimator(fieldLayout,PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_LAST_POSE,cameraFront,camerapose);
+    
+    // public static double GettargetDistance(){
+    //     double distance = cameraFront.getCameraTable().getEntry("targetPose").getDoubleArray(new double[] {0,0,0})[0];
+    //     System.out.println(distance);
+    //     return distance;
+    // }
 
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,7 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.NetworkTable;
+
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
