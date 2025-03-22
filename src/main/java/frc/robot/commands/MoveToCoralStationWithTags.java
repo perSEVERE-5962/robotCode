34b0f2f8a339113327b5f9472ca3dd16a0111ba6
Pranshu5962 <sharma.pranshu2009@gmail.com>
2025-03-22 14:28:54 -0400

    For Pranav and Kfir

--- src/main/java/frc/robot/commands/MoveToCoralStationWithTags.java
+++ src/main/java/frc/robot/commands/MoveToCoralStationWithTags.java
@@ -5,26 +5,27 @@
 package frc.robot.commands;
 
 import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.Constants.PhotonVisionConstant;
-import frc.robot.Constants.PhotonVisionConstant;
+
 import frc.robot.PhotonVision;
+
+import java.util.List;
+
+import org.photonvision.PhotonTargetSortMode;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class MoveToCoralStationWithTags extends Command {
- private boolean targetVisible =false;
- private double targetYaw = 0.0;
- private double targetRange = 0.0;
- private boolean targetVisible2 =false;
- private double targetYaw2 = 0.0;
- private double targetRange2 = 0.0;
- private double area=0.0;
+  private boolean targetVisible = false;
+  private double targetYaw = 0.0;
+  private double targetRange = 0.0;
 
- 
   /** Creates a new MoveToCoralStationWithTags. */
   public MoveToCoralStationWithTags() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -33,72 +34,81 @@ public class MoveToCoralStationWithTags extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    var results = PhotonVisionConstant.CameraNames[1].getAllUnreadResults();
-    var results2 = PhotonVisionConstant.CameraNames[0].getAllUnreadResults();
-    
-    if (!results.isEmpty()||!results2.isEmpty()) {
-     // Camera processed a new frame since last
-     // Get the last one in the list.
-     var result = results.get(results.size() - 1);
-     var result2 = results2.get(results.size() - 1);
-     if (result.hasTargets()||result2.hasTargets()) {
-         // At least one AprilTag was seen by the camera
-         for (var target : result.getTargets()) {
-             if (target.getFiducialId() == 9 || target.getFiducialId() == 6) {
-                 // Found Tag 7, record its information
-                 if(target.getArea()>area){
-                 targetYaw = target.getYaw();
-                 targetRange =
-                         PhotonUtils.calculateDistanceToTargetMeters(
-                                 0.5, // Measured with a tape measure, or in CAD.
-                                 0.3, // From 2024 game manual for ID 7
-                                 Units.degreesToRadians(-30.0), // Measured with a protractor, or in CAD.
-                                 Units.degreesToRadians(target.getPitch()));
-                 
- 
-                 targetVisible = true;
-                 area=target.getArea();
-                 }
-
-
-             }
-             for (var target2 : result.getTargets()) {
-              if (target2.getFiducialId() == 9 || target2.getFiducialId() == 6) {
-                  // Found Tag 7, record its information
-                  if(target2.getArea()>area){
-                  targetYaw = target.getYaw();
-                  targetRange =
-                          PhotonUtils.calculateDistanceToTargetMeters(
-                                  0.5, // Measured with a tape measure, or in CAD.
-                                  0.3, // From 2024 game manual for ID 7
-                                  Units.degreesToRadians(-30.0), // Measured with a protractor, or in CAD.
-                                  Units.degreesToRadians(target.getPitch()));
-                  
-  
-                  targetVisible = true;
-                  area=target.getArea();
-                  }
- 
- 
-              }
-         }
-     }
- }
-}
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    PhotonPipelineResult results = PhotonVisionConstant.CameraNames[1].getLatestResult();
+    PhotonPipelineResult results2 = PhotonVisionConstant.CameraNames[2].getLatestResult();
+    List<PhotonTrackedTarget> targets = results.getTargets();
+    List<PhotonTrackedTarget> targets2 = results2.getTargets();
+    targets.sort(PhotonTargetSortMode.Highest.getComparator());
+    targets2.sort(PhotonTargetSortMode.Highest.getComparator());
+
+    if (targets.isEmpty() && targets.isEmpty()) {
+
+      // Turn lights
+    } else if (targets.isEmpty() || targets2.isEmpty()) {
+      if (!targets.isEmpty()) {
+        targetYaw = targets.get(0).getYaw();
+        targetRange = PhotonUtils.calculateDistanceToTargetMeters(
+            0.35,
+            0.31,
+            Units.degreesToRadians(0),
+            Units.degreesToRadians(targets.get(0).getPitch()));
+        targetVisible = true;
+      } else if (!targets2.isEmpty()) {
+        targetYaw = targets2.get(0).getYaw();
+        targetRange = PhotonUtils.calculateDistanceToTargetMeters(
+            0.35,
+            0.31,
+            Units.degreesToRadians(0),
+            Units.degreesToRadians(targets2.get(0).getPitch()));
+        targetVisible = true;
+      }
+
+    } else if (!targets.isEmpty() && !targets.isEmpty()) {
+      if (targets.get(0).getFiducialId() != targets2.get(0).getFiducialId()
+          && targets.get(0).getArea() < targets2.get(0).getArea()) {
+        targetYaw = targets2.get(0).getYaw();
+        targetRange = PhotonUtils.calculateDistanceToTargetMeters(
+            0.35,
+            0.31,
+            Units.degreesToRadians(0),
+            Units.degreesToRadians(targets2.get(0).getPitch()));
+
+        targetVisible = true;
+        System.out.println(targetRange + "Range");
+        System.out.println(targetYaw + "Yaw");
+      } else if (targets.get(0).getFiducialId() != targets2.get(0).getFiducialId()
+          && targets.get(0).getArea() > targets2.get(0).getArea()) {
+        targetYaw = targets.get(0).getYaw();
+        targetRange = PhotonUtils.calculateDistanceToTargetMeters(
+            0.35,
+            0.31,
+            Units.degreesToRadians(0),
+            Units.degreesToRadians(targets.get(0).getPitch()));
+
+      } else if (targets.get(0).getFiducialId() == targets2.get(0).getFiducialId()) {
+        // Change to do averages
+        targetYaw = targets.get(0).getYaw();
+        targetRange = PhotonUtils.calculateDistanceToTargetMeters(
+            0.35,
+            0.31,
+            Units.degreesToRadians(0),
+            Units.degreesToRadians(targets.get(0).getPitch()));
+
+      }
+
+    }
 
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
-   
-  
+
   }
 
   // Returns true when the command should end.
