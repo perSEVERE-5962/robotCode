430daf23a37497e89fbed5cb7e0894d05a6feb20
Pranshu5962 <sharma.pranshu2009@gmail.com>
2025-05-27 19:52:23 -0400

    work on 5/27

--- src/main/java/frc/robot/commands/SetReachPosition.java
+++ src/main/java/frc/robot/commands/SetReachPosition.java
@@ -35,7 +35,6 @@ public class SetReachPosition extends Command {
   @Override
   public void execute() {
    
-    reachSub.moveToPositionWithPID(ScoringConstants.postions[targetPos][ScoringConstants.kReach]);
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -46,24 +46,33 @@ public class Pivot extends Actuator {
   }
 
   public void periodic() {
-        double theEncoder=instance.getPosition();
+        double theEncoder=getPosition();
        SmartDashboard.putNumber("Pivot", theEncoder);
   }
-  public void moveToPositionWithPID(double position) {
+  public void moveToPositionWithPID(double targetPosition) {
     SparkMaxConfig motorConfig = getMotorConfig();
-    if(getPosition() - position > 0){
-      motorConfig.inverted(true);      
-      getArmMotor().configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-      followerConfig.inverted(false);
-      followerMotor.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    }
-    else{
+    double currentPosition = getPosition();
+    SmartDashboard.putNumber("Pivot target position", targetPosition);
+       SmartDashboard.putNumber("Pivot source position",currentPosition);
+
+  
+    
+    // if((currentPosition - targetPosition) > 0){
+    //   SmartDashboard.putString("Pivot State", ">0");
+    //   motorConfig.inverted(true);      
+    //   getArmMotor().configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    //   followerConfig.inverted(false);
+    //   followerMotor.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // }
+     if ((currentPosition - targetPosition) <= 0){
+      SmartDashboard.putString("Pivot State", "<=0");
       motorConfig.inverted(false);
       getArmMotor().configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
       followerConfig.inverted(true);
       followerMotor.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
-    getArmMotor().getClosedLoopController().setReference(position, SparkMax.ControlType.kPosition);
+  getArmMotor().getClosedLoopController().setReference(targetPosition, SparkMax.ControlType.kPosition);
+    
   }
 
   public static Pivot getInstance() {
